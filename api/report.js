962f72463589363c099f1879728fe86af0282e08
Shaurya Jain <shauryajm2818@gmail.com>
2026-04-01 13:23:55 +0530

    Trigger Vercel redeploy

--- api/report.js
+++ api/report.js
@@ -10,11 +10,9 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-
   try {
     const { type, image, location, note } = req.body;
     
